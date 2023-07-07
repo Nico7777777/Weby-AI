da106a465f8740d34dd9d53c75aadd2bb4266cf8
dacosoftsolution <carmicialexandru@yahoo.com>
2023-07-08 02:40:40 +0300

    webyai

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from tkinter import *
 import openai
+from src.scrappy import extracter
+from os import path
+from os import mkdir
 
 
 openai.api_key="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"
 
 models = openai.Model.list()
+
+isSend = False
 
 # --------------------VARIABLES---------------------
 root = Tk()
@@ -16,7 +21,7 @@ FONT = "Helvetica 14"
 FONT_BOLD = "Helvetica 13 bold"
 
 root.title("WebyAI")
-lable1 = Label(root, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome", font=FONT_BOLD, pady=10, width=20, height=1).grid(
+lable1 = Label(root, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome to WebyAI", font=FONT_BOLD, pady=10, width=20, height=1).grid(
     row=0)
 
 txt = Text(root, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=60)
@@ -30,34 +35,37 @@ e.grid(row=2, column=0)
 
 #---------------------FUNCTIONS---------------------
 def send():
+    isSend = True
     input = e.get()
     send = "You -> " + input
     txt.insert(END, "\n" + send)
-    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": input}])
+    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": input},
+                                                                                    {"role": "system", "content": "you give me only the html, css and js for my requirement about a website without any messages and comments"}])
     user = e.get().lower()
-    txt.insert(END, "\n" + "Bot -> " + chat_completion.choices[0].message.content)
+    txt.insert(END, "\n" + "Bot -> Your command is done successfully! :)")
+    if not path.exists("predeploy"):
+        mkdir("predeploy")
+    with open(path.join("predeploy", "input.html"), "a") as f:
+        f.write(extracter(chat_completion.choices[0].message.content, "html"))
+    css_code = extracter(chat_completion.choices[0].message.content, "css")
+    with open(path.join("predeploy", "style.css"), "a") as f:
+        f.write(css_code)
+    with open(path.join("predeploy", "styles.css"), "a") as f:
+        f.write(css_code)
+    with open(path.join("predeploy", "script.js"), "a") as f:
+        f.write(extracter(chat_completion.choices[0].message.content, "javascript"))
     e.delete(0, END)
 
-def output_data():
-    with open("modele.txt", 'w') as f:
-        f.write(str(models))
-
-def output_engines():
-    with open("motoare.txt", 'w') as g:
-        for alpha in range(len(models.data)):
-            g.write( str(alpha+1) + '. ' + str(models.data[alpha].id) + '\n')
 #----------------------------------------------------
 
 send = Button(root, text="Send", font=FONT_BOLD, bg=BG_GRAY,
             command=send).grid(row=2, column=1)
 
 def main():
+    root.resizable(False, False)
     root.mainloop()
 
 
 if __name__=="__main__":
-    #output_data()
-    #output_engines()
-    gpt_turbo = models.data[16].id # gpt-3.5-turbo
-    #print()
+    gpt_turbo = models.data[16].id
     main()
---
+++ predeploy/script.js
@@ -0,0 +1,2 @@
+
+// Add any JavaScript functionality here if needed
