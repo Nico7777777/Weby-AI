ad77b42be6dfd5b8073ee979c3a43efa6e2a8af5
dacosoftsolution <carmicialexandru@yahoo.com>
2023-07-08 21:07:54 +0300

    weby

--- predeploy/script.js
+++ predeploy/script.js
@@ -1,1 +1,2 @@
- a HTML, CSS, and JS code template for you to create a modern and responsive website with a design similar to Netflix:
+
+// No JavaScript functionality mentioned
--- src/deploy.py
+++ src/deploy.py
@@ -2,4 +2,4 @@ from os import system
 from os import path
 
 def weby_deploy(linkId):
-    system("cd predeploy && netlify login && netlify link " + linkId + " && netlify deploy --dir=" + path.join(".") + " --prod --alias=WEBLYAI_WEBSITE")
+    system("cd predeploy && netlify login && netlify link --id " + linkId + " && netlify deploy --dir=" + path.join(".") + " --prod --alias=WEBLYAI_WEBSITE")
